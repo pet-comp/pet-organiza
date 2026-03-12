f183c57bb28b1e0a3f2d2c0d29614585018d0e1b
Kattryel Rezende <kattryel.rezende123@yahoo.com>
2026-03-12 14:46:53 -0300

    Integração inicial com Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "pet-organiza", appId: "1:156099391045:web:b2c8dee6abab6496d5bd2c", storageBucket: "pet-organiza.firebasestorage.app", apiKey: "AIzaSyCPDCaYSSf-o5Myq71RY5X9KwsTY3EKcQA", authDomain: "pet-organiza.firebaseapp.com", messagingSenderId: "156099391045", measurementId: "G-P0F5RL0W1R", projectNumber: "156099391045", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCPDCaYSSf-o5Myq71RY5X9KwsTY3EKcQA",
+    authDomain: "pet-organiza.firebaseapp.com",
+    projectId: "pet-organiza",
+    storageBucket: "pet-organiza.firebasestorage.app",
+    messagingSenderId: "156099391045",
+    appId: "1:156099391045:web:b2c8dee6abab6496d5bd2c",
+    measurementId: "G-P0F5RL0W1R"
+  }
 };
