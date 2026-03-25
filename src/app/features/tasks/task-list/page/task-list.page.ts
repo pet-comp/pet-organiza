a51b4c7415575086a7a64085d68c62baae60f527
Luisgzanetti <luisguilhermezanetti@gmail.com>
2026-03-24 21:43:31 -0300

    fix(task-list): card de categorias corrigido
    Arrumei um detalhe nos cards de categoria, agora mostram corretamente o número de tarefas e o número de tarefas completadas.

--- src/app/features/tasks/task-list/page/task-list.page.ts
+++ src/app/features/tasks/task-list/page/task-list.page.ts
@@ -28,6 +28,24 @@ export class TaskListPage implements OnInit {
       name: "Saúde",
       icon: "pulse-outline",
       colorHue: 220,
+    },
+    {
+      id: "cat_3",
+      name: "Trabalho",
+      icon: "briefcase-outline",
+      colorHue: 0,
+    },
+    {
+      id: "cat_4",
+      name: "Exercício",
+      icon: "barbell-outline",
+      colorHue: 30,
+    },
+    {
+      id: "cat_5",
+      name: "Playstation",
+      icon: "logo-playstation",
+      colorHue: 230,
     }
   ];
 
@@ -35,7 +53,16 @@ export class TaskListPage implements OnInit {
     { id: 't_1', name: 'Revisar lista de Álgebra Linear', completed: true, categoryId: 'cat_1' },
     { id: 't_2', name: 'Mandar e-mail pra graduação', completed: false, categoryId: 'cat_1' },
     { id: 't_3', name: 'Fazer a ata da reunião', completed: false, categoryId: 'cat_1' },
-    { id: 't_4', name: 'Ir no médico da UPA', completed: false, categoryId: 'cat_2' }
+    { id: 't_4', name: 'Ir no médico da UPA', completed: false, categoryId: 'cat_2' },
+    { id: 't_5', name: 'Tomar remédio', completed: false, categoryId: 'cat_2' },
+    { id: 't_6', name: 'Participar da reunião de trabalho', completed: false, categoryId: 'cat_3' },
+    { id: 't_7', name: 'Criar o novo endpoint', completed: false, categoryId: 'cat_3' },
+    { id: 't_8', name: 'Cobrar salário atrasado', completed: false, categoryId: 'cat_3' },
+    { id: 't_9', name: 'Ir à academia', completed: false, categoryId: 'cat_4' },
+    { id: 't_10', name: 'Correr 15km', completed: false, categoryId: 'cat_4' },
+    { id: 't_11', name: 'Fazer Terra com 425kg', completed: false, categoryId: 'cat_4' },
+    { id: 't_12', name: 'Jogar Division Rivals semanal no FIFA', completed: false, categoryId: 'cat_5' },
+    { id: 't_13', name: 'Farmar 5.000.000 no GTA', completed: false, categoryId: 'cat_5' },
   ];
 
   constructor() { }
@@ -45,6 +72,10 @@ export class TaskListPage implements OnInit {
 
   getTasksByCategory(categoryId: string) {
     return this.allTasks.filter(task => task.categoryId === categoryId);
+  }
+
+  getCompletedTasksByCategory(categoryId: string) {
+    return this.getTasksByCategory(categoryId).filter(task => task.completed).length;
   }
 
   navigateToCreateTask() {
