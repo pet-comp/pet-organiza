7181d92e469bfaa433cc4019e90e92573daeaa12
Kattryel Rezende <kattryel.rezende123@yahoo.com>
2026-03-26 15:17:48 -0300

    Criação da tela de galeria de cubos + Adição de ícones dos cubos

--- src/app/features/cubes/cubes-list/page/cubes-list.page.ts
+++ src/app/features/cubes/cubes-list/page/cubes-list.page.ts
@@ -11,10 +11,27 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule],
 })
 export class CubesListPage implements OnInit {
+  selectedCategory: string = 'todos';
+  arestasSaldo: number = 150;
+
+  cubes = [
+    { id: 1, name: 'de Gelo', rarity: 'comuns', img: 'assets/cubes/ice.svg', locked: false },
+    { id: 2, name: 'Gamer', rarity: 'comuns', img: 'assets/cubes/gamer.svg', locked: false },
+    { id: 3, name: 'Nerd', rarity: 'comuns', img: 'assets/cubes/nerd.svg', locked: false },
+    { id: 4, name: 'Punk', rarity: 'raros', img: 'assets/cubes/punk.svg', locked: false },
+    { id: 5, name: 'Kids', rarity: 'comuns', img: 'assets/cubes/kids.svg', locked: false },
+    { id: 6, name: 'Bruxo', rarity: 'raros', img: 'assets/cubes/wizard.svg', locked: true },
+    { id: 7, name: 'Penguin', rarity: 'lendarios', img: 'assets/cubes/penguin.svg', locked: true },
+    { id: 8, name: 'Mágico', rarity: 'raros', img: 'assets/cubes/rubiks.svg', locked: true },
+    { id: 9, name: 'Terrestre', rarity: 'lendarios', img: 'assets/cubes/earth.svg', locked: true },
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  get filteredCubes() {
+    if (this.selectedCategory === 'todos') return this.cubes;
+    return this.cubes.filter(cube => cube.rarity === this.selectedCategory);
+  }
 }
