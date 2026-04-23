25b41fdfeb4246590e54e5206dd12e61fb9d6bbf
Luisgzanetti <luisguilhermezanetti@gmail.com>
2026-04-23 17:14:44 -0300

    Criada a tela(nao implementada) e a rota para new category e new-habit

---
+++ src/app/features/tasks/new-category/page/new-category.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-category',
+  templateUrl: './new-category.page.html',
+  styleUrls: ['./new-category.page.scss'],
+})
+export class NewCategoryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
